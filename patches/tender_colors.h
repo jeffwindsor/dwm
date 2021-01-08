1eb83bbcaaf27b6b70d3280d2479d6c0dec2e5c9
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-07 16:15:49 -0800

    refactor

---
+++ patches/tender_colors.h
@@ -0,0 +1,27 @@
+// tender theme colors
+static const char col_blue1[]       = "#b3deef";
+static const char col_blue2[]       = "#73cef4";
+static const char col_blue3[]       = "#44778d";
+static const char col_blue4[]       = "#335261";
+static const char col_blue5[]       = "#293b44";
+static const char col_green1[]      = "#c9d05c";
+static const char col_green3[]      = "#6a6b3f";
+static const char col_green4[]      = "#464632";
+static const char col_yellow1[]     = "#d3b987";
+static const char col_yellow2[]     = "#ffc24b";
+static const char col_yellow3[]     = "#715b2f";
+static const char col_red1[]        = "#f43753";
+static const char col_red2[]        = "#c5152f";
+static const char col_red3[]        = "#79313c";
+static const char col_highlight[]   = "#ffffff";
+static const char col_text[]        = "#eeeeee";
+static const char col_pearl[]       = "#dadada";
+static const char col_gandolf[]     = "#bbbbbb";
+static const char col_grey1[]       = "#999999";
+static const char col_grey2[]       = "#666666";
+static const char col_grey3[]       = "#444444";
+static const char col_shadow[]      = "#323232";
+static const char col_bg[]          = "#282828";
+static const char col_dark[]        = "#202020";
+static const char col_darker[]      = "#1d1d1d";
+static const char col_darkest[]     = "#000000";
