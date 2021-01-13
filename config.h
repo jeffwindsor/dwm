8338ff2832dcde9a86af605b559c3faedecba88e
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-13 14:02:20 -0800

    colors

--- config.h
+++ config.h
@@ -9,21 +9,19 @@
 #include "push.c"
 
 // bar
-static const char *tags[]       = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int showbar        = 1;        /* 0 means no bar */
 static const int topbar         = 1;        /* 0 means bottom bar */
+static const char *tags[]       = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *fonts[]      = { "JetBrains Mono Light:size=12" }; //{ "Noto Sans Mono:size=11"} 
 static const char *colors[][3]  = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_blue2, col_bg, col_bg },
-	[SchemeSel]  = { col_bg, col_blue3, col_blue3 },
-	//[SchemeNorm] = { col_blue2, col_blue5, col_blue4 },
-	//[SchemeSel]  = { col_yellow2, col_blue4, col_blue4 },
+	[SchemeNorm] = { col_blue1, col_bg, col_bg },
+	[SchemeSel]  = { col_blue1, col_blue4, col_blue4 },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg    border     */
-	[SchemeNorm] = { 0xff, 0xdd, 0xdd },
-	[SchemeSel]  = { 0xff, 0xdd, 0xdd },
+	[SchemeNorm] = { 0xff, 0xbb, 0xbb },
+	[SchemeSel]  = { 0xff, 0xbb, 0xbb },
 };
 
 // dmenu
@@ -32,7 +30,7 @@ static const char *dmenucmd[] = { "dmenu_run",
     "-m", dmenumon, 
     "-fn", "JetBrains Mono Light:size=12", 
     "-nf", col_green1, "-nb", col_bg,
-    "-sf", col_green1, "-sb", col_green3, 
+    "-sf", col_green1, "-sb", col_green4, 
     NULL };
 
 // systray
@@ -146,7 +144,7 @@ static const char *const autostart[] = {
     "variety", NULL,
     "xfce4-power-manager", NULL,
     "volumeicon", NULL,
-    "blueberry-tray", NULL,
+    "greenberry-tray", NULL,
     "ssh-agent -s", NULL,   
     "/usr/lib/xfce4/notifyd/xfce4-notifyd", NULL,
     "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1", NULL,
