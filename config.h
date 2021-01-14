8c7933b892f07274796472d9651c10eec3880ba4
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-13 23:13:18 -0800

    font awesome fallback

--- config.h
+++ config.h
@@ -12,7 +12,10 @@
 static const int showbar        = 1;        /* 0 means no bar */
 static const int topbar         = 1;        /* 0 means bottom bar */
 static const char *tags[]       = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *fonts[]      = { "JetBrains Mono Light:size=12" }; //{ "Noto Sans Mono:size=11"} 
+static const char *fonts[]      = { 
+    "JetBrains Mono Light:size=12",
+    "FontAwesome:style=Regular:size=12"
+}; //{ "Noto Sans Mono:size=11"} 
 static const char *colors[][3]  = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_blue1, col_bg, col_bg },
