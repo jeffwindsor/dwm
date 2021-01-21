3bbb682109cd81a136c0d9d273c2160deda02dc5
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-20 16:32:52 -0800

    Refactor

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const unsigned int systraypinning = 0;     /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 0;     /* systray spacing */
 static const int systraypinningfailfirst = 1;     /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray             = 1;     /* 0 means no systray */
+static const int showsystray             = 0;     /* 0 means no systray */
 
 /* windows */
 static const unsigned int borderpx       = 1;     /* border pixel of windows */
