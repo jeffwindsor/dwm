39a893ae3f4d42ddbdf61f64c5f918e2ba66f746
Jeff Windsor <jeff.windsor@gmail.com>
2022-01-09 14:07:31 -0800

    personal colors

---
+++ colors_tender.h
@@ -0,0 +1,38 @@
+// tender theme colors
+static const char col_blue1[]       = "#b3deef";
+static const char col_blue2[]       = "#73cef4";
+static const char col_blue3[]       = "#44778d";
+static const char col_blue4[]       = "#293b44";
+static const char col_blue5[]       = "#212f36";
+static const char col_blue6[]       = "#192329";
+static const char col_blue7[]       = "#10181b";
+
+static const char col_green1[]      = "#c9d05c";
+static const char col_green3[]      = "#6a6b3f";
+static const char col_green4[]      = "#464632";
+static const char col_green5[]      = "#282a12";
+
+static const char col_yellow1[]     = "#ffc24b";
+static const char col_yellow2[]     = "#cc9b3c";
+static const char col_yellow3[]     = "#99742d";
+static const char col_yellow4[]     = "#664e1e";
+static const char col_yellow5[]     = "#33270f";
+
+static const char col_red1[]        = "#f43753";
+static const char col_red2[]        = "#c5152f";
+static const char col_red3[]        = "#760d1c";
+static const char col_red4[]        = "#4f0813";
+static const char col_red5[]        = "#270409";
+
+static const char col_highlight[]   = "#ffffff";
+static const char col_text[]        = "#eeeeee";
+static const char col_pearl[]       = "#dadada";
+static const char col_gandolf[]     = "#bbbbbb";
+static const char col_grey1[]       = "#999999";
+static const char col_grey2[]       = "#666666";
+static const char col_grey3[]       = "#444444";
+static const char col_shadow[]      = "#323232";
+static const char col_bg[]          = "#282828";
+static const char col_dark[]        = "#202020";
+static const char col_darker[]      = "#1d1d1d";
+static const char col_darkest[]     = "#000000";
--- config.def.h
+++ config.def.h
@@ -1,30 +1,24 @@
 /* See LICENSE file for copyright and license details. */
+#include "colors_tender.h"      /* color scheme */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int startwithgaps	     = 0;	 /* 1 means gaps are used by default */
+static const int startwithgaps	    = 0;	 /* 1 means gaps are used by default */
 static const unsigned int gappx     = 10;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
+static const char dmenufont[]       =   "monospace:size=10";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_blue1, col_blue7, col_blue4 },
+    [SchemeSel]  = { col_blue1, col_blue5, col_blue3 },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeNorm] = { OPAQUE, 0xbb, OPAQUE },
+	[SchemeSel]  = { OPAQUE, 0xbb, OPAQUE },
 };
 
 /* tagging */
@@ -66,8 +60,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
 
 #include "selfrestart.c"
 
